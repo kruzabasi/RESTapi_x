c0a1cf6ba86b55a9e63f3787d374bc0d74dbec68
kruzabasi <kruzabasi@gmail.com>
2020-05-20 17:00:31 +0100

    database created, http verbs defined via postman

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,18 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
-    },
-    "extends": "eslint:recommended",
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-        "ecmaVersion": 11,
-        "sourceType": "module"
-    },
-    "rules": {
-    }
+  env: {
+    browser: true,
+    es6: true
+  },
+  extends: "airbnb-base",
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly"
+  },
+  parserOptions: {
+    ecmaVersion: 11,
+    sourceType: "module"
+  },
+  rules: {
+    "comma-dangle": 0
+  }
 };
--- app.js
+++ app.js
@@ -1,19 +1,49 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
 const app = express();
+const db = mongoose.connect("mongodb://localhost/bookAPI");
 const bookRouter = express.Router();
 const port = process.env.PORT || 3000;
+const Book = require("./models/bookModel");
 
-bookRouter.route("/books").get((req, res) => {
-  const response = { hello: "This is my api..." };
-  res.json(response);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+bookRouter
+  .route("/books")
+  .get((req, res) => {
+    const query = {};
+    if (req.query.genre) {
+      query.genre = req.query.genre;
+    }
+    Book.find(query, (err, books) => {
+      if (err) {
+        return res.send(err);
+      }
+      return res.json(books);
+    });
+  })
+  .post((req, res) => {
+    const book = new Book(req.body);
+    book.save();
+    return res.status(201).json(book);
+  });
+bookRouter.route("/books/:bookId").get((req, res) => {
+  Book.findById(req.params.bookId, (err, book) => {
+    if (err) {
+      return res.send(err);
+    }
+    return res.json(book);
+  });
 });
 app.use("/api", bookRouter);
 
 app.get("/", (req, res) => {
-  res.send("Welcome to my Api");
+  res.send("Welcome to my Nodemon API!");
 });
 
 app.listen(port, () => {
-  console.log(`Running on ${port}`);
+  console.log(`Running on port ${port}`);
 });
