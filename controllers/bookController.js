45a0130dd8f2d4e80e10ce320c480c190b200388
kruzabasi <kruzabasi@gmail.com>
2020-05-21 20:56:49 +0100

    controller logic separated

---
+++ controllers/bookController.js
@@ -0,0 +1,21 @@
+function bookController(Book) {
+  function get(req, res) {
+    const query = {};
+    if (req.query.genre) {
+      query.genre = req.query.genre;
+    }
+    Book.find(query, (err, books) => {
+      if (err) {
+        return res.send(err);
+      }
+      return res.json(books);
+    });
+  }
+  function post(req, res) {
+    const book = new Book(req.body);
+    book.save();
+    return res.status(201).json(book);
+  }
+  return { post, get };
+}
+module.exports = bookController;
